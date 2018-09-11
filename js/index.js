c1896add036b39d0f3d921f6d508e37116b0454f
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-11 16:46:59 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ function getRepositories() {
 function displayRepositories() {
   var repos = JSON.parse(this.responseText)
   const repoList = `<ul>` + repos.forEach(function(element) {
+    debugger;
     return `<li> <a href="${element.html_url}">${element.name}</a></li>`
   }) + `</ul>`
   document.getElementById("repositories").innerHTML = repoList
