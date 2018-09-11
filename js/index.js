5075ad86414141765661b5c2eea8d83bc0fea83e
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-11 20:28:30 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -25,7 +25,7 @@ function displayRepositories() {
 function getCommits(el) {
   var commitReq = new XMLHttpRequest()
   commitReq.addEventListener('load', displayCommits)
-  commitReq.open('GET', `https://api.github.com/repos/${el.dataset.repoowner}/${el.dataset.reponame}/commits`)
+  commitReq.open('GET', `https://api.github.com/repos/${el.dataset.owner}/${el.dataset.name}/commits`)
   commitReq.send()
 }
 
