644ac0002334bc9c720eb74057e08e8dd80ccbb7
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-11 20:59:37 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -13,9 +13,9 @@ function displayRepositories() {
   repoList += repos.map(repo =>
     `<li>
       <a href="${repo.html_url}">${repo.name}</a> -
-      <a href="#" data-owner="${repo.owner.login}" data-name="${repo.name}" onclick="getCommits(this)">
+      <a href="#" data-username="${repo.owner.login}" data-repositary="${repo.name}" onclick="getCommits(this)">
       Get Commits</a>
-      <a href="##" data-owner="${repo.owner.login}" data-name="${repo.name}" onclick="getBranches(this)">
+      <a href="##" data-username="${repo.owner.login}" data-repositary="${repo.name}" onclick="getBranches(this)">
       Get Branches</a>
     </li>`
   ).join()
@@ -25,14 +25,14 @@ function displayRepositories() {
 function getCommits(el) {
   var commitReq = new XMLHttpRequest()
   commitReq.addEventListener('load', displayCommits)
-  commitReq.open('GET', `https://api.github.com/repos/${el.dataset.owner}/${el.dataset.name}/commits`)
+  commitReq.open('GET', `https://api.github.com/repos/${el.dataset.username}/${el.dataset.repository}/commits`)
   commitReq.send()
 }
 
 function getBranches(el) {
   var req = new XMLHttpRequest()
   req.addEventListener('load', displayBranches)
-  req.open('GET', `https://api.github.com/repos/${el.dataset.owner}/${el.dataset.name}/branches`)
+  req.open('GET', `https://api.github.com/repos/${el.dataset.username}/${el.dataset.repository}/branches`)
   req.send()
 }
 
